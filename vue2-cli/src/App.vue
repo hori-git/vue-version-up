2185cf6ba9a8a01888d1c1bf3280b4baad64ad00
Keisuke <horihoribass@gmail.com>
2023-08-19 14:37:51 +0900

    Vue2 CDNとVue2 CLI

---
+++ vue2-cli/src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="app">
+    <!-- 入力画面 -->
+    <div v-if="!showConfirmation">
+      <input v-model="message" />
+      <button @click="toggleConfirmation">確認</button>
+    </div>
+
+    <!-- 確認画面 -->
+    <div v-if="showConfirmation">
+      <p>入力したメッセージ: {{ message }}</p>
+      <button @click="toggleConfirmation">戻る</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  el: "#app",
+  data: function () {
+    return {
+      message: "",
+      showConfirmation: false,
+    };
+  },
+  methods: {
+    toggleConfirmation() {
+      this.showConfirmation = !this.showConfirmation;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Arial", sans-serif;
+  width: 300px;
+  margin: 50px auto;
+  padding: 20px;
+  border: 1px solid #ddd;
+  box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.1);
+}
+
+input {
+  padding: 8px;
+  margin-bottom: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+button {
+  padding: 8px 15px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #0056b3;
+}
+
+p {
+  margin-bottom: 10px;
+}
+</style>
